528431f15870026338886c2a2f5fff5c82445f7c
gabrielasalvo <gabrielamdesalvo@gmail.com>
2020-08-10 17:56:51 -0300

    verifyGeometricFigure

---
+++ 2.js
@@ -0,0 +1,19 @@
+/**02) Os triângulos podem ser classificados em 3 tipos quanto ao tamanho de seus lados:
+Equilátero: Os três lados são iguais. Isósceles: Dois lados iguais. Escaleno: Todos os lados são diferentes.
+Crie uma função que recebe os comprimentos dos três lados de um triângulo e retorne sua classificação quanto
+ao tamanho de seus lados. (Neste exemplo deve-se abstrair as condições matemáticas de existência de um
+triângulo). */
+
+function verifyGeometricFigure(a,b,c) {
+    if(a === b && b === c ){
+        console.log("Seu triângulo é Equilátero")
+    }else if (a === b || b ===c || c === a ) {
+        console.log("Seu triangulo é Isósceles")
+        
+    } else {
+        console.log("Seu triâgulo é Escaleno")
+        
+    }
+
+}
+verifyGeometricFigure(2,2,2)
