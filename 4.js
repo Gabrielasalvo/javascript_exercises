c44e0d4259f26564669fa23c479aeebc7ad89bee
gabrielasalvo <gabrielamdesalvo@gmail.com>
2020-08-10 18:26:02 -0300

    3

---
+++ 4.js
@@ -0,0 +1,9 @@
+/*
+03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente
+* */
+function exponencial(base, factor) {
+
+   console.log(Math.pow(base,factor))
+}
+
+exponencial(4,4)
